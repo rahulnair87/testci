474510e8abf6445e558c5ae0dc874e14855a0593
Rahul <rahulnair87@gmail.com>
2022-08-01 12:12:00 +0530

    tuscan

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,12 @@
 const sum = require("../functions/sum");
+const path = require("path");
+const fs = require("fs");
 
 test("adds 1 + 2 to equal 3", () => {
+
+  const filePath = path.join(__dirname, '..', 'YoroiBuilds/Yoroi Nightly/manifest.json');
+  const str = fs.readFileSync(filePath);
+  console.log(str);
   expect(sum(1, 2)).toBe(3);
 });
+
